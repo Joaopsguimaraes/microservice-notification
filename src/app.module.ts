7d63ee8df73d0cc1210e0a1256d343b3049a68dd
Joao Guimaraes <psjoaovictor@gmail.com>
2022-12-12 21:46:58 -0300

    feat: ✨ first class about nestjs

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { PrismaService } from './prisma.service';
+import { AppController } from './app.controller';
+import { HttpModule } from './http.module';
+import { Module } from '@nestjs/common';
+
+@Module({
+  imports: [HttpModule],
+  controllers: [AppController],
+  providers: [PrismaService],
+})
+export class AppModule {}
